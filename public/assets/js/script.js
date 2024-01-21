1a85f982e31e00e5e9bca52c60a36d932e97b92c
Shikuku39 <enshikuku39@gmail.com>
2024-01-22 01:15:35 +0300

    l=styling and added item effect

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,13 @@
 let cartCounter = document.querySelector('#cart-counter')
 let cartItems = document.querySelector('#cart-items')
+let added = document.querySelector('.item-added')
 var cartid = []
 function addinputandcartcount(id) {
     cartCounter.innerHTML = parseInt(cartCounter.innerHTML) + 1
+    added.style.display = 'block'
+    setTimeout(function() {
+        added.style.display = 'none'
+    }, 2000)
     if (cartid.includes(id)) {
         let input = document.querySelector(`input[name="${id}"]`)
         input.value = parseInt(input.value) + 1
