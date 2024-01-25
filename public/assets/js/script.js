efeb33cb0905a0c3d047f3e827920e751f814835
Shikuku39 <enshikuku39@gmail.com>
2024-01-25 14:31:36 +0300

    updated addcartcounter function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,23 +1,9 @@
 let cartCounter = document.querySelector('#cart-counter')
-let cartItems = document.querySelector('#cart-items')
 let added = document.querySelector('.item-added')
-var cartid = []
-function addinputandcartcount(id) {
-    cartCounter.innerHTML = parseInt(cartCounter.innerHTML) + 1
+function addcartcount() {
     added.style.display = 'block'
     setTimeout(function() {
         added.style.display = 'none'
     }, 2000)
-    if (cartid.includes(id)) {
-        let input = document.querySelector(`input[name="${id}"]`)
-        input.value = parseInt(input.value) + 1
-        return
-    } else {
-        let input = document.createElement('input')
-        input.type = 'hidden'
-        input.value = 1
-        input.name = id
-        cartItems.appendChild(input)
-        cartid.push(id)
-    }
+    cartCounter.innerHTML = parseInt(cartCounter.innerHTML) + 1
 }
