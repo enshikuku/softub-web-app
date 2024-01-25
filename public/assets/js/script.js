eb0f24ed8ce0afbfdb7e98d8440de4c82e597c48
Shikuku39 <enshikuku39@gmail.com>
2024-01-25 14:59:38 +0300

    Implement dynamic cart count update based on selected quantity in addcartcount() function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,39 @@
 let cartCounter = document.querySelector('#cart-counter')
 let added = document.querySelector('.item-added')
 function addcartcount() {
+    let selectedQuantity = parseInt(document.getElementById('quantity').value)
+    
     added.style.display = 'block'
     setTimeout(function() {
         added.style.display = 'none'
-    }, 2000)
-    cartCounter.innerHTML = parseInt(cartCounter.innerHTML) + 1
+    }, 4000)
+
+    cartCounter.innerHTML = parseInt(cartCounter.innerHTML) + selectedQuantity
+}
+
+function addToCart(event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const formObject = {}
+    
+    formData.forEach((value, key) => {
+        formObject[key] = value
+    })
+
+    fetch('/add-to-cart', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formObject)
+    })
+    .then(response => response.json()) 
+    .then(data => {
+        console.log(data)
+        
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    })
 }
